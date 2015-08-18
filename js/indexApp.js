c704f82f9e152a484981edb0897a7997b9ac78e4
Yaroslav Dvinov <yd43@cornell.edu>
2015-08-18 00:49:20 -0700

    Refactoring to comply with best practices guide at https://github.com/johnpapa/angular-styleguide

--- js/indexApp.js
+++ js/indexApp.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('indexApp', [ 'googleApi' ])
-    .config(function(googleLoginProvider) {
-      googleLoginProvider.configure({
-        clientId : '187173892432-dnsafu5beq2j4qgggl8kf7serfca9umn.apps.googleusercontent.com',
-        scopes : [ 'email' ]
-      });
-    })
+  .config(function(googleLoginProvider) {
+    googleLoginProvider.configure({
+      clientId : '187173892432-dnsafu5beq2j4qgggl8kf7serfca9umn.apps.googleusercontent.com',
+      scopes : [ 'email' ]
+    });
+  })
   .controller('googleController', ['$scope', 'googleLogin', 'googlePlus', function ($scope, googleLogin, googlePlus) {
-    $scope.login = function () {
+    var vm = this;
+    
+    console.log('controllerLoaded');
+    vm.login = function () {
         googleLogin.login()
           .then(function() {
             console.log("login success");
@@ -18,12 +21,12 @@ angular.module('indexApp', [ 'googleApi' ])
     };
     
     $scope.$on("google:authenticated", function(auth) {
-      $scope.loggedIntoGoogle = true;
+      vm.loggedIntoGoogle = true;
     });
     
     $scope.$on("googlePlus:loaded", function apiClientLoaded() {
       googlePlus.getCurrentUser().then(function(user) {
-        $scope.clientName = user.name.givenName;
+        vm.clientName = user.name.givenName;
       });
     });
   }]);
