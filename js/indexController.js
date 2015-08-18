19baa4cd075a7ad5c9e8f513f318385ee1748493
Yaroslav Dvinov <yd43@cornell.edu>
2015-08-17 20:18:15 -0700

    Migrating to angular-googleapi for Google auth

--- js/indexController.js
+++ js/indexController.js
@@ -1,20 +1,39 @@
-angular.module('indexApp', [ 'directive.g+signin' ]).controller(
-    'indexController',
-    function($scope) {
-      console.log('Controller loaded');
+'use strict';
 
-      $scope.$on('event:google-plus-signin-success',
-          function(event, authResult) {
-            gapi.client.load('plus', 'v1', apiClientLoaded)
+angular.module('indexApp', [ 'googleApi' ])
+    .config(function(googleLoginProvider) {
+      googleLoginProvider.configure({
+        clientId : '187173892432-dnsafu5beq2j4qgggl8kf7serfca9umn.apps.googleusercontent.com',
+        scopes : [ 'email' ]
+      });
+    })
+  .controller('indexController', function($scope, googleLogin) {
+    console.log('Controller loaded');
+
+    $scope.login = function () {
+        googleLogin.login()
+          .then(function() {
+            console.log("login success");
+          }, function() {
+            console.error("failed login");
           });
-
-      function apiClientLoaded() {
-        gapi.client.plus.people.get({
-          userId : 'me'
-        }).execute(handleResponse);
-      }
-
-      function handleResponse(resp) {
-        console.log(resp);
-      }
+    };
+    
+    $scope.$on("google:authenticated", function(auth) {
+      $scope.loggedIntoGoogle = true;
+      gapi.client.load('plus', 'v1', apiClientLoaded);
     });
+    
+    function apiClientLoaded() {
+      gapi.client.plus.people.get({
+        userId : 'me'
+      }).execute(handleResponse);
+    }
+
+    function handleResponse(resp) {
+      console.log(resp);
+      $scope.clientName = resp.displayName;
+      $scope.$apply();
+    }
+  }
+);
